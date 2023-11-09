5628cc82e5e4cd67fd088c944051b9e2c57c3083
Jiaming Situ <112342179+Jiaming-S@users.noreply.github.com>
2023-11-09 13:00:48 -0800

    drag top bar

--- scripts.js
+++ scripts.js
@@ -10,3 +10,33 @@ document.addEventListener('scroll', () => {
   }
 });
 
+let isDragging = false;
+let curTranslate = 0;
+const topBarInternal = document.querySelector('.top-bar-internal');
+document.addEventListener('mousedown', () => isDragging = true);
+document.addEventListener('mouseup', () => isDragging = false);
+document.addEventListener('mousemove', (e) => {
+  if (isDragging) {
+    const dx = e.movementX;
+    curTranslate += dx;
+    topBar.style.transform = `translateX(${curTranslate}px)`;
+  }
+});
+
+const downArrow = document.querySelector('#down-arrow');
+downArrow.addEventListener('click', () => {
+  window.scrollBy({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
+});
+
+const upArrow = document.querySelector('#up-arrow');
+upArrow.addEventListener('click', () => {
+  window.scrollBy({
+    top: -window.innerHeight,
+    behavior: 'smooth'
+  });
+});
+
+
