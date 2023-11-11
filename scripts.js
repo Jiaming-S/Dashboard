bde8d886a216b6347d3de99e54c0d2871d857670
Jiaming Situ <112342179+Jiaming-S@users.noreply.github.com>
2023-11-10 19:28:34 -0800

    progress bar and topbar

--- scripts.js
+++ scripts.js
@@ -2,24 +2,11 @@
 const topBar = document.querySelector('.top-bar');
 document.addEventListener('scroll', () => {
   curScroll = window.scrollY;
-  if (curScroll > 10) {
+  if (curScroll > 50) {
     topBar.classList.add('scrolled');
   }
   else {
     topBar.classList.remove('scrolled');
-  }
-});
-
-let isDragging = false;
-let curTranslate = 0;
-const topBarInternal = document.querySelector('.top-bar-internal');
-document.addEventListener('mousedown', () => isDragging = true);
-document.addEventListener('mouseup', () => isDragging = false);
-document.addEventListener('mousemove', (e) => {
-  if (isDragging) {
-    const dx = e.movementX;
-    curTranslate += dx;
-    topBar.style.transform = `translateX(${curTranslate}px)`;
   }
 });
 
@@ -38,5 +25,46 @@ upArrow.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+
+const collegeInfo = {
+  cornell: {
+    name: "cornell",
+    timeSubmitted: "11/01/2023",
+    timeResponse: "12/12/2023"
+  },
+  purdue: {
+    name: "purdue", 
+    timeSubmitted: "11/01/2023",
+    timeResponse: "01/15/2024"
+  },
+  northeastern: {
+    name: "northeastern",
+    timeSubmitted: "11/01/2023",
+    timeResponse: "01/28/2024"
+  }
+}
+const allProgressBars = document.querySelectorAll('.progress');
+
+setTimeout(function updateProgressBars() {
+  allProgressBars.forEach((bar) => {
+    const collegeName = bar.id.split('-')[0];
+    const info = collegeInfo[collegeName];
+    const timeSubmitted = new Date(info.timeSubmitted);
+    const timeResponse = new Date(info.timeResponse);
+    const now = new Date();
+    const total = timeResponse - timeSubmitted;
+    const elapsed = now - timeSubmitted;
+    const percent = Math.round(100 * elapsed / total);
+    bar.querySelector('.progress-bar').style.width = `${percent}%`;
+    bar.querySelector('.progress-bar').innerHTML = `${percent}%`;
+    bar.parentElement.querySelector('.progress-labels > p:nth-child(1)').innerHTML = timeSubmitted.toLocaleDateString();
+    bar.parentElement.querySelector('.progress-labels > p:nth-child(2)').innerHTML = "~" + timeResponse.toLocaleDateString();
+  });
+}, 300);
+
+
+
+
 
 
