ae3fb5b30e07876e0e50e487cf1016614c1b8b2d
Jiaming Situ <112342179+Jiaming-S@users.noreply.github.com>
2023-11-25 17:32:36 -0800

    consistency

--- generate.js
+++ generate.js
@@ -1,10 +1,10 @@
 
 const wrapper = document.querySelector('.item-wrapper');
+const rowNum = Object.keys(collegeInfo).length;
 
 setTimeout(function populateCollegeInfo() {
   const placeholders = document.querySelectorAll('.item-wrapper > .placeholder');
   const placeholderNum = placeholders.length;
-  const rowNum = Object.keys(collegeInfo).length;
   if (placeholderNum !== rowNum) {
     if (placeholderNum > rowNum) {
       for (let i = rowNum; i < placeholderNum; i++) wrapper.removeChild(placeholders[i]);
@@ -20,8 +20,8 @@ setTimeout(function populateCollegeInfo() {
       name: collegeInfo[curKey].name,
       id: collegeInfo[curKey].id,
       appType: collegeInfo[curKey].appType,
-      timeSubmitted: collegeInfo[curKey].timeSubmitted,
-      timeResponse: collegeInfo[curKey].timeResponse,
+      dateSubmitted: collegeInfo[curKey].dateSubmitted,
+      dateResponse: collegeInfo[curKey].dateResponse,
       collegePortalWebsite: collegeInfo[curKey].collegePortalWebsite,
     }
     const curRow = placeholders[i];
@@ -29,33 +29,37 @@ setTimeout(function populateCollegeInfo() {
     curRow.id = `${curInfo.id}`;
   
     if (useIFrame) curRow.querySelector('iframe').src = curInfo.collegePortalWebsite;
-  
-    curRow.querySelectorAll('.item-small')[0].querySelector('h2').textContent = curInfo.name;
-    curRow.querySelectorAll('.item-small')[0].querySelector('p').textContent = `Submitted ${curInfo.timeSubmitted}`;
-  
-    curRow.querySelectorAll('.item-small')[1].querySelector('h3').textContent = curInfo.appType;
-  
-    curRow.querySelectorAll('.item-medium')[0].querySelector('.progress').id = `${curInfo.id}-progress`;
+    curRow.querySelector('.college-name').textContent = curInfo.name;
+    curRow.querySelector('.college-submission-date').textContent = `Submitted ${curInfo.dateSubmitted}`;
+    curRow.querySelector('.college-application-type').textContent = curInfo.appType;
+    curRow.querySelector('.progress').id = `${curInfo.id}-progress`;
   }
 }, populationDelay);
 
 setTimeout(function updateProgressBars() {
   const allProgressBars = document.querySelectorAll('.progress');
-  allProgressBars.forEach((bar) => {
-    const collegeName = bar.id.split('-')[0];
-    const info = collegeInfo[collegeName];
-    const timeSubmitted = new Date(info.timeSubmitted);
-    const timeResponse = new Date(info.timeResponse);
+  for (let i = 0; i < rowNum; i++) {
+    const curKey = Object.keys(collegeInfo)[i];
+    const curInfo = {
+      dateSubmitted: collegeInfo[curKey].dateSubmitted,
+      dateResponse: collegeInfo[curKey].dateResponse,
+    }
+    const curBar = allProgressBars[i];
+
+    const dateSubmitted = new Date(curInfo.dateSubmitted);
+    const dateResponse = new Date(curInfo.dateResponse);
     const now = new Date();
-    const total = timeResponse - timeSubmitted;
-    const elapsed = now - timeSubmitted;
+    const total = dateResponse - dateSubmitted;
+    const elapsed = now - dateSubmitted;
     const percent = Math.round(100 * elapsed / total);
-    bar.querySelector('.progress-bar').style.width = `${percent}%`;
-    bar.querySelector('.progress-bar').innerHTML = `${percent}%`;
-    bar.parentElement.querySelector('.progress-labels > p:nth-child(1)').innerHTML = timeSubmitted.toLocaleDateString();
-    bar.parentElement.querySelector('.progress-labels > p:nth-child(2)').innerHTML = "~" + timeResponse.toLocaleDateString();
-  });
-}, progressBarDelay);
+
+    curBar.querySelector('.progress-bar').style.width = `${percent}%`;
+    curBar.querySelector('.progress-bar').textContent = `${percent}%`;
+    
+    curBar.parentElement.querySelector('.progress-labels > p:nth-child(1)').textContent = dateSubmitted.toLocaleDateString();
+    curBar.parentElement.querySelector('.progress-labels > p:nth-child(2)').textContent = "~" + dateResponse.toLocaleDateString();
+  };
+}, updateProgressDelay);
 
 
 
--- meta.js
+++ meta.js
@@ -1,31 +1,31 @@
 
 const useIFrame = false;
 const populationDelay = 500;
-const progressBarDelay = 1000;
+const updateProgressDelay = 1000;
 
 const collegeInfo = {
   cornell: {
     name: "Cornell University",
     id: "cornell",
     appType: "Early Decision",
-    timeSubmitted: "11/01/2023",
-    timeResponse: "12/12/2023",
+    dateSubmitted: "11/1/2023",
+    dateResponse: "12/12/2023",
     collegePortalWebsite: "https://engage.admissions.cornell.edu/account/login",
   },
   purdue: {
     name: "Purdue University",
     id: "purdue",
     appType: "Early Action",
-    timeSubmitted: "11/01/2023",
-    timeResponse: "01/15/2024",
+    dateSubmitted: "11/1/2023",
+    dateResponse: "1/15/2024",
     collegePortalWebsite: "https://www.admissions.purdue.edu/apply/applicationstatus.php",
   },
   northeastern: {
     name: "Northeastern University",
     id: "northeastern",
     appType: "Early Action",
-    timeSubmitted: "11/01/2023",
-    timeResponse: "01/28/2024",
+    dateSubmitted: "11/1/2023",
+    dateResponse: "1/28/2024",
     collegePortalWebsite: "https://apply.northeastern.edu/portal/app_status?utm_campaign=FRapplicant&utm_content=ASC_appacknowledge&utm_medium=email&utm_source=deliver&utm_term=fa24",
   },
 };
